a4c3b462549ef3390e32fc518a9560487725db85
MochammadTaufikJuliandi <taufik.juliandi4@gmail.com>
2023-04-04 08:21:49 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbars from './components/Navbar';
+import Hero from './components/Hero';
+
+function App() {
+  return (
+    <>
+    <Navbars />
+    <Hero />
+    </>
+  );
+}
+
+export default App;
