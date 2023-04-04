86a8ef332f76c5499f0f603c3ad349f09148ed47
MochammadTaufikJuliandi <taufik.juliandi4@gmail.com>
2023-04-04 09:04:18 +0700

    update content

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbars from './components/Navbar';
 import Hero from './components/Hero';
+import Main from './components/Main';
 
 function App() {
   return (
     <>
     <Navbars />
     <Hero />
+    <Main />
     </>
   );
 }
