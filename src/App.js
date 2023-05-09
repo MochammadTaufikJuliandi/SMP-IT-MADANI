a805f1ef1812f26b656829ad897f20260630c9af
MochammadTaufikJuliandi <taufik.juliandi4@gmail.com>
2023-05-09 21:05:43 +0700

    update navbar

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Main from './components/Main';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
+    <BrowserRouter>
     <>
     <Navbar />
     <Hero />
     <Main />
     </>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import '../style/Navbar.css';
+import { Link }from "react-router-dom";
 
 function Navbar() {
   return (
@@ -12,9 +13,9 @@ function Navbar() {
   </div>
   <nav id="navigationDrawer" className="app-bar__navigation">
     <ul>
-      <li><a href="#/now-playing">Beranda</a></li>
-      <li><a href="#/upcoming">Tentang</a></li>
-      <li><a href="#/like">Kegiatan</a></li>
+      <li><Link to="#/beranda">Beranda</Link></li>
+      <li><Link to="#/tentang">Tentang</Link></li>
+      <li><Link to="#/kegiatan">Kegiatan</Link></li>
     </ul>
   </nav>
 </header>
